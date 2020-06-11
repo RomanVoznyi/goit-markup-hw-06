132cb0179a78ae0f596bf34d5bde7d277ad3d8a2
Roman Voznyy <roman.voznyy.52@gmail.com>
2020-06-11 10:44:38 +0300

    created modal-form

---
+++ js/modal.js
@@ -0,0 +1,29 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-open-modal]"),
+    closeModalBtn: document.querySelector("[data-close-mod-btn]"),
+    closeModalLink: document.querySelector("[data-close-mod-link]"),
+    backdrop: document.querySelector("[data-backdrop]"),
+  };
+
+  // ---- Open and close Modal ------
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalLink.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.backdrop.classList.toggle("is-hidden");
+  }
+
+  // ----- Display date to console ------
+  document.querySelector(".js-modal").addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    new FormData(e.currentTarget).forEach((value, name) =>
+      console.log(`${name}: ${value}`)
+    );
+
+    e.currentTarget.reset();
+  });
+})();
